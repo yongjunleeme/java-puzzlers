cf7ccca2abe9d266ce43178c0b6d280db1745db3
yongjunleeme <chesidyong@gmail.com>
2022-06-21 08:36:20 +0900

    Update 18

---
+++ src/main/java/Puzzle18/Answer.java
@@ -0,0 +1,13 @@
+package Puzzle18;
+
+public class Answer {
+
+    public static void main(String[] args) throws Exception {
+        byte[] bytes = new byte[256];
+        for (int i = 0; i < 256; i++)
+            bytes[i] = (byte) i;
+        String str = new String(bytes, "ISO-8859-1" );
+        for (int i = 0, n = str.length(); i < n; i++)
+            System.out.print((int) str.charAt(i) + " " );
+    }
+}
