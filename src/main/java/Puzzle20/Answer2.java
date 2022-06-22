64ca57b1689da1b0ef6705a03ab6a924b4fc47b3
yongjunleeme <chesidyong@gmail.com>
2022-06-22 09:02:04 +0900

    Update 19, 20

---
+++ src/main/java/Puzzle20/Answer2.java
@@ -0,0 +1,11 @@
+package Puzzle20;
+
+import java.util.regex.Pattern;
+
+public class Answer2 {
+
+    public static void main(String[] args) {
+        System.out.println(Me.class.getName().
+                replaceAll(Pattern.quote("."), "/") + ".class");
+    }
+}
